b518610fd9745082eec314ede09f501e8af3a032
asadovskaya <sashasadovskaya@gmail.com>
2020-05-17 22:19:55 +0300

    separated AddTodo

---
+++ src/Modal/Modal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import 'Modal.css'
+
+export default class Modal extends React.Component{
+    render() {
+        return(
+            <React.Fragment>
+                <button>Open modal</button>
+
+                <div className='modal'>
+                    <div className='modal-body'>
+                        <h1>Modal title</h1>
+                        <p>I am awesome modal</p>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
